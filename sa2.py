b7c397f3c822c9513325b3a9d30c1b78b58b68ce
ashut0sh99 <120705765+ashut0sh99@users.noreply.github.com>
2022-12-22 23:36:33 +0530

    Add files via upload

---
+++ sa2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pickle
+import streamlit as st
+
+model=pickle.load(open('sentiment_analysis_model.p','rb'))
+
+st.title(' Sentiment Analysis Model ')
+
+st.write('Enter text for sentiment analysis:')
+message = st.text_area("","Type Here ...")
+if st.button('PREDICT'):
+    disp=" "
+    a = model.predict([message])[0]
+    if(a == 'pos'):
+        disp = "Positive Sentiment!"
+    else:
+        disp = "Negative Sentiment!"
+    st.write('The sentiment of given text is:', disp)
+    # st.header(f"**{a}**")
+    # q = model.predict_proba([message])
+
+
+# In[ ]:
+
+
+
+
